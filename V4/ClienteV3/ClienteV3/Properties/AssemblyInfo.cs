70fb2cf9b50d05216efeba572c94af9e9c2f080f
Marc Rubí <marc.rubi@estudiantat.upc.edu>
2025-05-07 17:15:51 +0200

    Versión 4

--- V4/ClienteV3/ClienteV3/Properties/AssemblyInfo.cs
+++ V4/ClienteV3/ClienteV3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("ClienteV3")]
+[assembly: AssemblyTitle("ClienteV4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClienteV3")]
+[assembly: AssemblyProduct("ClienteV4")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
